649791f9bc8e1a8912b24d27b4852d747c3ccf75
Julio Cesar A <julio@vixnegocios.com.br>
2020-06-23 17:11:26 -0300

    Modificação para concluir o desafio 05

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,53 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = { income: 0, outcome: 0, total: 0 };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    this.balance = { income: 0, outcome: 0, total: 0 };
+    this.transactions.map(transaction => {
+      if (transaction.type === 'income') {
+        this.balance.income += Number(transaction.value);
+      }
+      if (transaction.type === 'outcome') {
+        this.balance.outcome += Number(transaction.value);
+      }
+    });
+
+    this.balance.total = this.balance.income - this.balance.outcome;
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    if (transaction.type === 'outcome') {
+      this.balance = this.getBalance();
+      if (this.balance.total < transaction.value)
+        throw Error('Without balance to carry out the transaction');
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
